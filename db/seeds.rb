f5472cc384233ec7ba06d289bd87486d80999346
Ken Murakami <headiv94@gmail.com>
2017-04-30 14:13:39 +0900

    レベル設定をseeds.rbで管理できるようにする

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+TimeLimitSingleLevelInfo.create(level: 1, length: 10, time: 50)
+TimeLimitSingleLevelInfo.create(level: 2, length: 10, time: 40)
+TimeLimitSingleLevelInfo.create(level: 3, length: 10, time: 30)
+TimeLimitSingleLevelInfo.create(level: 4, length: 10, time: 20)
+TimeLimitSingleLevelInfo.create(level: 5, length: 10, time: 10)
+TimeLimitSingleLevelInfo.create(level: 6, length: 20, time: 50)
+TimeLimitSingleLevelInfo.create(level: 7, length: 20, time: 40)
+TimeLimitSingleLevelInfo.create(level: 8, length: 20, time: 30)
+TimeLimitSingleLevelInfo.create(level: 9, length: 20, time: 20)
+TimeLimitSingleLevelInfo.create(level: 10, length: 20, time: 10)
